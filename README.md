3eea2cfd48a0713a873fab7af2f6a046ec5e2728
Benja Efigenio <befigenio@gmail.com>
2019-05-11 10:29:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# clarity-forms-force
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LCC from 'lightning-container';
+
+const title = 'Clarity Forms App Exchange';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
