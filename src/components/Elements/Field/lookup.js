3e45e94af701ab5ff378d8895596f63db5053773
Benjamin Efigenio <befigenio@Benjamins-MacBook-Pro.local>
2019-10-09 22:43:13 -0500

    lookup is displayed on preview

--- src/components/Elements/Field/lookup.js
+++ src/components/Elements/Field/lookup.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Select, Spin } from 'antd';
 
 const Option = Select.Option;
 
 export const Lookup = ({ question }) => {
 
+    const [search, setSearch] = useState(false);
+    const [selection, setSelection] = useState('');
+    const [newResults, setNewResults] = useState([]);
+
+    const onChange = () => {
+
+    }
+
+    const onRemoveOption = () => {
+
+    }
+
+    const onSelect = () => {
+
+    }
+
     return [
-        'Lookup'
+        <Select
+            mode="multiple"
+            labelInValue
+            placeholder={question.Placeholder__c}
+            notFoundContent={search ? <Spin size="small" /> : null}
+            filterOption={false}
+            onChange={(value) => onSelect(value)}
+            onSearch={(value) => onChange(value)}
+            onDeselect={(value) => onRemoveOption(value)}
+            style={{ width: '100%' }}
+        >
+            {newResults.map(d => <Option key={d.id}>{d.label}</Option>)}
+        </Select>
     ]
 
 }
