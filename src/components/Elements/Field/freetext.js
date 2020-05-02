3bf146577ec10edeef3200503b1dd79d6b0bcd1b
Benjamin Efigenio <befigenio@gmail.com>
2020-05-02 15:25:54 -0500

    add freetext style

--- src/components/Elements/Field/freetext.js
+++ src/components/Elements/Field/freetext.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const FreeText = ({ question }) => {
     
 	return question.forms__FreeText_Type__c == 'Header' ?
-	<div className="slds-text-heading_large">{question.forms__Title__c}</div> :
+	<div className="slds-section__title slds-theme--shade primaryPaletteBorder">
+		<span className="section-header-title slds-p-horizontal--small slds-truncate">
+			{question.forms__Title__c}
+		</span>
+	</div> :
 	<div className="slds-text-body_small">{question.forms__Title__c}</div>
 
 }
